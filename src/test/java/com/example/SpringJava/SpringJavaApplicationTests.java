bbb2a2f958f0f77942ef46ee63eb84c9ea8381a9
Ninh Vi <ninhvvbg26@gmail.com>
2022-07-17 00:06:37 +0700

    update

---
+++ src/test/java/com/example/SpringJava/SpringJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
